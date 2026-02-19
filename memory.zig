9c9050bd6b1e27e7432095fc97c010786006b1d8
Haris Sattar <harissattar41@gmail.com>
2026-02-19 02:18:58 -0800

    added some memory functionality

--- memory.zig
+++ memory.zig
@@ -1,11 +1,28 @@
 const MEMORY_SIZE = 0x10000;
 
-const CARTRIDGE_ROM_START: u16 = 0x0000;
-const CARTRIDGE_ROM_END: u16 = 0x8000;
+pub const MemoryMap = struct {
+    pub const ROM0: AddressRange = .{ .start = 0x0000, .end = 0x3FFF };
+    pub const ROM1_BANKED: AddressRange = .{ .start = 0x4000, .end = 0x7FFF };
+    pub const VRAM: AddressRange = .{ .start = 0x8000, .end = 0x9FFF };
+    pub const SRAM: AddressRange = .{ .start = 0xA000, .end = 0xBFFF };
+    pub const WRAM0: AddressRange = .{ .start = 0xC000, .end = 0xCFFF };
+    pub const WRAM1: AddressRange = .{ .start = 0xD000, .end = 0xDFFF };
+    pub const ECHO_RAM: AddressRange = .{ .start = 0xE000, .end = 0xFDFF };
+    pub const OAM: AddressRange = .{ .start = 0xFE00, .end = 0xFE9F };
+    pub const IO: AddressRange = .{ .start = 0xFF00, .end = 0xFF7F };
+    pub const HRAM: AddressRange = .{ .start = 0xFF80, .end = 0xFFFE };
+    pub const IE: u16 = 0xFFFF;
+};
+
+pub const AddressRange = struct {
+    start: u16,
+    end: u16,
+};
 
 pub const Memory = struct {
     map: [MEMORY_SIZE]u8 = undefined,
     boot_rom_enabled: bool = true,
+    // TODO: need a cartridge
 
     pub fn init() Memory {
         return Memory{
@@ -14,7 +31,7 @@ pub const Memory = struct {
     }
 
     pub fn read(self: *const @This(), address: u16) u8 {
-        if (address <= 0xBFFF) {
+        if (address >= MemoryMap.ROM0.start) {
             // read from cartridge
             return;
         } else if (address == 0xFF00) {
